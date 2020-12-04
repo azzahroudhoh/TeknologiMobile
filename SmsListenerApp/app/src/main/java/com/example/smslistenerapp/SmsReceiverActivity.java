71b989854dbb5a06de566aa275f9198d07b2325e
azzahroudhoh <91azzahroudhoh@gmail.com>
2020-12-04 09:35:57 +0700

    ARSITEKU

--- SmsListenerApp/app/src/main/java/com/example/smslistenerapp/SmsReceiverActivity.java
+++ SmsListenerApp/app/src/main/java/com/example/smslistenerapp/SmsReceiverActivity.java
@@ -1,14 +1,14 @@
-package com.example.arsiteku;
+package com.example.smslistenerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SplashScreen extends AppCompatActivity {
+public class SmsReceiverActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        setContentView(R.layout.activity_sms_receiver);
     }
 }
